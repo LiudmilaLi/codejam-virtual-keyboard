a10d7b039a8f9863850f0587d76d1a13579f0e00
LiudmilaLi <LiudmilaLitvin@gmail.com>
2019-11-06 21:57:14 +0300

    fix: delete node_modules

--- index.js
+++ index.js
@@ -1,66 +1,45 @@
 'use strict';
 
-const keys =  ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-      'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\',
-      'Caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
-      'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '∧', 'Shift',
-      'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl'
-    ];
+let keyboard = document.createElement('div');
+keyboard.className = 'keyboard';
+document.body.append(keyboard);
+let input = document.createElement('textarea');
+input.className = 'input';
+document.body.prepend(input);
 
-   /* eng: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-      'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
-      'Caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
-      'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '∧', 'Shift',
-      'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl'
-    ]
-  },
+const keysCode = [['BackQuote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5',
+  'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO',
+  'KeyP', 'BracketLeft', 'BracketRight'],
+['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL',
+  'Semicolon', 'Quote', 'Backslash', 'Enter'],
+['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period',
+  'Slash', 'ArrowUp', 'ShiftRight'],
+['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft',
+  'ArrowDown', 'ArrowRight']];
 
-  shift: false,
-  caps: false,
-  lang: 'eng',
-  alt: false
-};*/
+const keyRus = [['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+  ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
+  ['Caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+  ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '∧', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl']];
 
-let div1 = document.createElement('textarea');
-div1.className = 'div_one';
-document.body.appendChild(div1);
-
-let div2 = document.createElement('div');
-div2.className = 'keyboard';
-document.body.appendChild(div2);
+const keyEng = [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+  ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+  ['Caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+  ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '∧', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl']];
 
 
-  function init() {
-    let str = '';
-    for (let i = 0; i < keys.length; i += 1) {
-      if (i === 15) {
-        str += '<div class = "clearfix"></div>';
-      }
-      str += '<div class="keyboard_keys">' + String(keys[i]) + '</div>';
-      
-    }
-  
-    document.querySelector('.keyboard').innerHTML = str;
+for (let i = 0; i < 5; i += 1) {
+  keyboard.insertAdjacentHTML('beforeEnd', '<div class="row"></div>');
+  let str = document.getElementsByClassName('row');
+  for (let j = 0; j < keyEng[i].length; j += 1) {
+    str[i].insertAdjacentHTML('beforeEnd', `<div id=${keysCode[i][j]} class="button">${keyEng[i][j]}</div>`);
   }
-  init();
+}
 
-  document.onkeypress = function (event) {
-    console.log(event.code);
-    console.log(event.keyCode);
-    document.querySelectorAll('.keyboard .keyboard_keys').forEach(function (element) {
-      element.classList.remove('active');
-    });
-    document.querySelector('.keyboard .keyboard_keys[data = "' + event.keyCode +'"]').classList.add('active');
-  }
-  
-  document.querySelectorAll('.keyboard .keyboard_keys').forEach(function (element) {
-    element.onclick = function (event) {
-      document.querySelectorAll('.keyboard .keyboard_keys').forEach(function(element) {
-        element.classList.remove('active');
-      });
-      let code = this.getAttribute('data');
-      this.classList.add('active');
-      console.log(code);
-    }
-  }); 
-  
+let buttons = document.getElementsByClassName('button');
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', btn);
+}
